67746e2364248255741b5cd5ee78ac252cc873c7
ui-fankfurt <ui.frankfurt@gmail.com>
2023-10-23 22:02:56 +0800

    adding new files

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Social = () => {
+    return (
+        <div className='home__social'>
+            <a href="https://www.linkedin.com/in/ui-ogino/" className="home__social-icon" 
+            target={"_blank"}>
+                <i class="uil uil-linkedin"></i>
+            </a>
+
+            <a href="https://github.com/ui-fankfurt" className="home__social-icon" 
+            target={"_blank"}>
+                <i class="uil uil-github-alt"></i>
+            </a>
+
+        </div>
+    )
+}
