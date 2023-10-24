320d89afacfaefe2e264818875425fc33eb98722
ui-fankfurt <ui.frankfurt@gmail.com>
2023-10-25 00:21:46 +0800

    adding new feature on home page

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Info = () => {
     return (
-    <div>Info</div>
+        <div className="about__info grid">
+            <div className="about__box">
+            <i class='bx bx-award about__icon'></i>
+                <h3 className="about__title">Experience</h3>
+                <span className="about__subtitle">2 Years Working</span>
+            </div>
+
+            <div className="about__box">
+            <i class='bx bx-briefcase-alt about__icon'></i>
+
+                <h3 className="about__title">Completed</h3>
+                <span className="about__subtitle">10 projects</span>
+            </div>
+            <br />
+        </div>
     );
 };
 
